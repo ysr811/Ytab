ef5f6e1dac1157a26b6f84a025498dfd312d25df
yasser8111 <yasserxd653@gmail.com>
2026-08-23 19:44:01 +0300

    Add index.js barrel export for modals module

---
+++ src/components/modals/index.js
@@ -0,0 +1,5 @@
+export { BaseModal } from "./BaseModal";
+export { CustomizeModal } from "./CustomizeModal";
+export { DeleteConfirmModal } from "./DeleteConfirmModal";
+export { EditSiteModal } from "./EditSiteModal";
+export { RenameModal } from "./RenameModal";
